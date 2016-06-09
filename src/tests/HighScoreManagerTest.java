b8f30ed57a66a70c95e94129b75a77ff415bdd79
polarny <sta.dawidowicz@gmail.com>
2016-06-09 22:54:27 +0200

    changes to high score test

--- src/tests/HighScoreManagerTest.java
+++ src/tests/HighScoreManagerTest.java
@@ -55,6 +55,78 @@ public class HighScoreManagerTest {
 
             Assert.assertEquals(expectedUserName, highScoreManager.getHighScoreName());
             Assert.assertEquals(expectedHighScore, highScoreManager.getHighScore());
+            Assert.assertEquals(expectedUserName, highScoreManager.getHighScoreName());
+            Assert.assertEquals(expectedHighScore, highScoreManager.getHighScore());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void badScoreTest(){
+        File f = new File(path + "\\" + fileName);
+        FileWriter output = null;
+
+        String properUserName = "User1";
+        int badScore = -2;
+        try {
+            output = new FileWriter(f);
+            BufferedWriter writer = new BufferedWriter(output);
+            writer.write(properUserName + " " + badScore);
+            writer.close();
+
+            HighScoreManager highScoreManager = new HighScoreManager(path);
+
+            Assert.assertEquals(properUserName, highScoreManager.getHighScoreName());
+            Assert.assertFalse(badScore == highScoreManager.getHighScore());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void badNameTest(){
+        File f = new File(path + "\\" + fileName);
+        FileWriter output = null;
+
+        String badUserName = "Space   Space.";
+        String userNameWithScore = "Cheater 66";
+        int properScore = 11;
+        int badScore = -1;
+
+        try {
+            output = new FileWriter(f);
+            BufferedWriter writer = new BufferedWriter(output);
+            writer.write(badUserName + " " + properScore);
+            writer.close();
+
+            HighScoreManager highScoreManager = new HighScoreManager(path);
+
+            Assert.assertEquals(properScore, highScoreManager.getHighScore());
+            Assert.assertEquals(badUserName, highScoreManager.getHighScoreName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void cheaterNameTest(){
+        File f = new File(path + "\\" + fileName);
+        FileWriter output = null;
+
+        String userNameWithScore = "Cheater 66";
+        int properScore = 11;
+
+        try {
+            output = new FileWriter(f);
+            BufferedWriter writer = new BufferedWriter(output);
+            writer.write(userNameWithScore + " " + properScore);
+            writer.close();
+
+            HighScoreManager highScoreManager = new HighScoreManager(path);
+
+            Assert.assertFalse(66 == highScoreManager.getHighScore());
+            Assert.assertEquals(properScore, highScoreManager.getHighScore());
         } catch (IOException e) {
             e.printStackTrace();
         }
