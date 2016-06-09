5675b69ba3c8e4f0bfb36d47d425acd95e984b8f
polarny <sta.dawidowicz@gmail.com>
2016-06-09 21:50:11 +0200

    changes to DistributionTest

--- src/tests/DistributionTest.java
+++ src/tests/DistributionTest.java
@@ -3,6 +3,8 @@ package tests;
 import app.Game;
 import org.junit.Test;
 
+import java.util.Random;
+
 /**
  * Created by Stach on 2016-06-09.
  */
@@ -15,22 +17,36 @@ public class DistributionTest {
                 //GUI Stuff
             }
         };
-
+        System.out.println("Only rock:");
         manyRuns(game, 1);
         displayscores(game);
         game.clearStats();
 
+        System.out.println("\nOnly paper:");
         manyRuns(game, 2);
         displayscores(game);
         game.clearStats();
 
+        System.out.println("\nOnly scissors:");
         manyRuns(game, 3);
+        displayscores(game);
+        game.clearStats();
+
+        System.out.println("\nRandom player choices:");
+        randomRuns(game);
         displayscores(game);
     }
 
     private static void manyRuns(Game game, int choice){
         for (int i=0; i<1000000; i++){
             game.play(choice);
+        }
+    }
+
+    private static void randomRuns(Game game){
+        Random random = new Random();
+        for (int i=0; i<1000000; i++){
+            game.play(random.nextInt(3) + 1);
         }
     }
 
